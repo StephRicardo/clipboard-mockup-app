6842e5a26461a5ff9dfd0a58b9a2ffa587e20b62
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2017-07-09 13:37:39 -0400

    Adds column title to designs, updates new design form and show views for paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611162038) do
+ActiveRecord::Schema.define(version: 20170709173545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170611162038) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.string "title"
     t.index ["user_id"], name: "index_designs_on_user_id"
   end
 
