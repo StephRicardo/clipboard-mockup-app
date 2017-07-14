9e849d3807496fbfe5af67feffc88d8ace35970f
Stephanie Ricardo <stephanie.ricardo1@gmail.com>
2017-07-13 23:28:14 -0400

    User can now star and unstar a design

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -1,10 +1,13 @@
 class StarsController < ApplicationController
-  respond_to :js
 
   def star
     @user = current_user
     @design = Design.find(params[:design_id])
     @user.star!(@design)
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def unstar
@@ -12,5 +15,9 @@ class StarsController < ApplicationController
     @star = @user.stars.find_by_design_id(params[:design_id])
     @design = Design.find(params[:design_id])
     @star.destroy!
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
